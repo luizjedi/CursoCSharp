defa0740da1a485ae6727800b7c96a6b8625d49e
luizjedi <luiz.pepe@hotmail.com>
2021-01-27 13:15:08 -0300

    Criação de Rotina

--- CursoCSharp/CursoCSharp/Program.cs
+++ CursoCSharp/CursoCSharp/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace CursoCSharp
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World! \nAmazing life!");
+using CursoCSharp.Fundamentos;
+
+namespace CursoCSharp {
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Comentários - Fundamentos", Comentarios.Executar },
+            });
+
+            central.SelecionarEExecutar();
         }
     }
 }
